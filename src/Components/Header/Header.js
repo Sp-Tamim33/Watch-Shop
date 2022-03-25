c0dfd4c21e3bff0e740876096a999ecdb31b327d
sp-tam1m <white.hat.tamim@gmail.com>
2022-03-25 22:50:00 +0600

    set up and header complete

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Welcome to Watch Shop</h1>
+            <h4>Choose Your Favorite Watch</h4>
+        </div>
+    );
+};
+
+export default Header;
